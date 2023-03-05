8f2be023591271892b35bc04b0ea1fde6288722c
Benjamin Lesne <benjamin.lesne@outlook.fr>
2023-03-05 18:49:02 +0100

    updated isObject type arg

--- object/isObject.ts
+++ object/isObject.ts
@@ -3,6 +3,6 @@
  *
  * return true if argument is an object 
  */
-export function isObject(item: any) {
+export function isObject(item: unknown) {
   return typeof item === "object" && !Array.isArray(item) && item !== null;
 }
