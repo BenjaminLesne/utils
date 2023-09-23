374f3082e5aff08709cd6a632695388f6392bf6c
Benjamin Lesne <benjamin.lesne@outlook.fr>
2023-09-23 15:47:29 +0200

    added assertNever

--- type.ts
+++ type.ts
@@ -1,4 +1,4 @@
-/**
+/*
  * Hover the PrettifyOn type and the PrettifyOff type.
  * It is much easier to understand what the nested type is in PrettifyOn compared to PrettifyOff type
  */
@@ -15,3 +15,12 @@ type PrettifyOff = PrettifyCore & { age: number };
 type PrettifyOn = Prettify<PrettifyCore & { age: number }>;
 
 // /Prettify type
+
+/*
+ * Inside a switch, we trigger this function in the default case, whenever we forget to exhaust all options this will error in TypeScript
+ * see: 3.3. Exhaustiveness Checking with the Assert never Technique #1 from TypeScript cookbook
+ */
+function assertNever(value: never) {
+  console.error("Unknown value", value);
+  throw Error("Not possible");
+}
